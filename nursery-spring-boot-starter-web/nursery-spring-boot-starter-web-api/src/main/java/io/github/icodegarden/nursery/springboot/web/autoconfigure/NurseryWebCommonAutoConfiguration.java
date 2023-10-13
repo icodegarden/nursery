8f920ac8a18a7d08e3d066d79329aadc3aeac81b
Fangfang.Xu <Fangfang.Xu@HZW001-W702WV0L.Geely.Auto>
2023-10-13 15:29:57 +0800

    add @ConditionalOnMissingBean

--- nursery-spring-boot-starter-web/nursery-spring-boot-starter-web-api/src/main/java/io/github/icodegarden/nursery/springboot/web/autoconfigure/NurseryWebCommonAutoConfiguration.java
+++ nursery-spring-boot-starter-web/nursery-spring-boot-starter-web-api/src/main/java/io/github/icodegarden/nursery/springboot/web/autoconfigure/NurseryWebCommonAutoConfiguration.java
@@ -1,6 +1,7 @@
 package io.github.icodegarden.nursery.springboot.web.autoconfigure;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,6 +27,7 @@ public class NurseryWebCommonAutoConfiguration {
 	@Configuration
 	protected static class MappingJackson2HttpMessageConverterAutoConfiguration {
 
+		@ConditionalOnMissingBean
 		@Bean
 		public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
 			log.info("nursery init bean of MappingJackson2HttpMessageConverter");
